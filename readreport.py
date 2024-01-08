596aec8f905889f3fbc26d47bf76c6b26bddfe39
bbejeck <bbejeck@gmail.com>
2024-01-07 20:30:42 -0500

    Add functionality to find largest increase in rides
    The code now includes a function to discern the largest increase in rides over a 10 year period. It reads data from a CSV file, filters them for a specific year range, counts rides for each route, and then sorts these in descending order. Additionally, the tracemalloc import and related lines have been commented out. A new readreport.py file has been included as well.

---
+++ readreport.py
@@ -0,0 +1,16 @@
+import csv
+
+
+def readportfolio(filename):
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            record = {
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+            portfolio.append(record)
+    return portfolio
--- readrides.py
+++ readrides.py
@@ -1,8 +1,12 @@
 import csv
 import sys
 from collections import namedtuple
+from collections import Counter
+from datetime import datetime
+from pprint import pprint
 
 Rowt = namedtuple('Rowt', ['route', 'date', 'daytype', 'rides'])
+date_format = '%m/%d/%Y'
 
 
 class Row:
@@ -63,9 +67,38 @@ def put_row_in_container(row, container):
         return Rowslot(route, date, daytype, rides)
 
 
+def find_largest_increase():
+    all_rides = read_rides_as('Data/ctabus.csv', 'class')
+    first_year = [row for row in all_rides if get_date(row.date).year == 2001]
+    year_one_counter = Counter()
+    for row in first_year:
+        year_one_counter[row.route] += row.rides
+    ten_year_counter = Counter()
+    ten_years = [row for row in all_rides if filter_by_year(get_date(row.date))]
+    for row in ten_years:
+        ten_year_counter[row.route] += row.rides
+    big_increase = []
+
+    for route, rides in year_one_counter.items():
+        big_increase.append((route, ten_year_counter[route] / rides))
+    
+    sorted_by_increase = sorted(big_increase, key=lambda r: int(r[1]), reverse=True)
+    pprint(sorted_by_increase[0:10])
+
+
+def get_date(dstr):
+    return datetime.strptime(dstr, date_format)
+
+
+def filter_by_year(date):
+    return 2001 <= date.year <= 2011
+
+
 if __name__ == '__main__':
-    import tracemalloc
-    container = sys.argv[1]
-    tracemalloc.start()
-    rows = read_rides_as('Data/ctabus.csv', container)
-    print(f'Memory Use for container type {container}:  {tracemalloc.get_traced_memory()}')
+    # import tracemalloc
+    #
+    # container = sys.argv[1]
+    # tracemalloc.start()
+    # rows = read_rides_as('Data/ctabus.csv', container)
+    # print(f'Memory Use for container type {container}:  {tracemalloc.get_traced_memory()}')
+    find_largest_increase()
